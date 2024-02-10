24a5d78456c6098a88a7782f9c1aeefbf56e59ca
stanislavbratkov <st.dm.br.04@mail.ru>
2024-02-10 16:09:16 +0300

    Tasks models

--- add_crm/add_crm/settings.py
+++ add_crm/add_crm/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,7 @@ ROOT_URLCONF = "add_crm.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
 TIME_ZONE = "UTC"
 
@@ -115,8 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'add_crm/static'),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
---
+++ add_crm/crm/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+
+class Task(models.Model):
+    """Задачи
+
+    Поля:
+        Название
+        Описание
+        Изображение
+        Дата создания
+        Дэдлайн
+        #todo Подумать про количество подзадач
+        Количество подзадач
+        Состояние выполненности
+    """
+    
+    class Meta:
+        db_table = "tasks"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+    
+    title = models.TextField(verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    img = models.ImageField(verbose_name="Изображение", blank=True, upload_to='photos/%Y/%m/%d/')
+    date_add = models.DateField(verbose_name="Дата создания", auto_now_add=True)
+    deadline = models.DecimalField(verbose_name="Дата сдачи", blank=True)
+    done = models.BooleanField(verbose_name="Выполнено")
+    
+    def __str__(self):
+        return self.title
+    
+
+class Subtask(models.Model):
+    """Подзадачи
+
+    Поля:
+        Название
+        Описание
+        Дата создания
+        Срок сдачи
+        Основная задача
+        Готовность
+    """
+    class Meta:
+        db_table = "subtasks"
+        verbose_name = "Подзадача"
+        verbose_name_plural = "Подзадачи"
+        
+    title = models.TextField(verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    date_add = models.DateField(verbose_name="Дата создания", auto_now_add=True)
+    deadline = models.DecimalField(verbose_name="Дата сдачи", blank=True)
+    main_task = models.ForeignKey(to="Task", on_delete=models.PROTECT)
+    done = models.BooleanField(verbose_name="Выполнено")
+    
