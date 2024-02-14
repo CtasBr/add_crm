0038ee41feff5fdabf216f50136e9bacd61f773f
stanislavbratkov <st.dm.br.04@mail.ru>
2024-02-14 18:38:07 +0300

    Calendar

--- add_crm/crm/views.py
+++ add_crm/crm/views.py
@@ -1,7 +1,32 @@
+import calendar
+import datetime
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 
 from .models import *
+
+
+def generate_calendar(month, year):
+    # Получаем количество дней в указанном месяце и году
+    num_days = calendar.monthrange(year, month)[1]
+    
+    # Получаем день недели первого дня указанного месяца
+    first_day_weekday = calendar.weekday(year, month, 1)
+    
+    # Создаем список-календарь с пустыми ячейками
+    calendar_list = [["" for _ in range(7)] for _ in range(5)]
+    
+    # Заполняем список-календарь днями месяца
+    current_day = 1
+    for week in range(6):
+        for weekday in range(7):
+            if current_day <= num_days and (week > 0 or weekday >= first_day_weekday):
+                calendar_list[week][weekday] = str(current_day)
+                current_day += 1
+    
+    return calendar_list
+
 
 
 def projects(request):
@@ -33,7 +58,6 @@ def tasks(request, num):
         deadline = request.POST.get('deadline')
         description = request.POST.get('description')
         users = request.POST.getlist('users')
-        print(title, deadline, description, users)
         task = Task(title=title, deadline=deadline, description=description, is_done=False, main_project_id=Project.objects.get(id=num))
         users = User.objects.filter(id__in=users)
         task.save()
@@ -48,7 +72,6 @@ def tasks(request, num):
         t_d.update(is_done=True)
     
     s_done_index = int(request.GET.get("done_subtask", -1))
-    print(s_done_index)
     if s_done_index != -1:
         s_d = Subtask.objects.filter(id=s_done_index)
         s_d.update(is_done=True)
@@ -60,22 +83,25 @@ def tasks(request, num):
     return render(request, 'tasks.html', data)
 
 def subtasks(request, num):
-    done_index = int(request.GET.get("done", -1))
-    if done_index > -1:
-        st_d = Subtask.objects.filter(id=done_index)
-        st_d.update(is_done=True)
-        
-    subtasks = Subtask.objects.filter(main_task_id=num)
-    data = {"subtasks": subtasks}
-    
-    return render(request, 'subtasks.html', data)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        deadline = request.POST.get('deadline')
+        subtask = Subtask(title=title, deadline=deadline, is_done=False, main_task_id=Task.objects.get(id=num))
+        subtask.save()
+        return redirect('tasks', subtask.main_task_id.main_project_id.id)
 
 def experiments(request):
     return render(request, 'experiments.html')
 
 
 def schedule(request):
-    return render(request, 'schedule.html')
+    now = datetime.datetime.now()
+
+    calendar = generate_calendar(int(now.month), int(now.year))
+    data = {"calendar": calendar}
+    
+    return render(request, 'schedule.html', data)
+
 
 
 # Create your views here.
