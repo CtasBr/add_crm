fbf8206ce167f50492a1b9ccf4aad024c2e1d2d7
stanislavbratkov <st.dm.br.04@mail.ru>
2024-02-14 13:42:51 +0300

    Updates

--- add_crm/crm/views.py
+++ add_crm/crm/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
 
 from .models import *
 
 
 def projects(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        deadline = request.POST.get('deadline')
+        description = request.POST.get('description')
+        print(title, deadline, description)
+        project = Project(title=title, deadline=deadline, description=description, is_done=False)
+        project.save()  # Сохраняем объект в базе данных
+
+        return redirect('projects')
+
+
+    
     done_index = int(request.GET.get("done", -1))
     if done_index > -1:
         p_d = Project.objects.filter(id=done_index)
@@ -15,13 +28,28 @@ def projects(request):
 
 
 def tasks(request, num):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        deadline = request.POST.get('deadline')
+        description = request.POST.get('description')
+        users = request.POST.getlist('users')
+        print(title, deadline, description, users)
+        task = Task(title=title, deadline=deadline, description=description, is_done=False, main_project_id=Project.objects.get(id=num))
+        users = User.objects.filter(id__in=users)
+        task.save()
+        task.executors_id.set(users)
+          # Сохраняем объект в базе данных
+
+        return redirect('tasks', num)
+    
     done_index = int(request.GET.get("done", -1))
     if done_index > -1:
         t_d = Task.objects.filter(id=done_index)
         t_d.update(is_done=True)
 
     tasks = Task.objects.filter(main_project_id=num)
-    data = {"tasks": tasks}
+    users = User.objects.all()
+    data = {"tasks": tasks, "users": users}
     return render(request, 'tasks.html', data)
 
 def subtasks(request, num):
