f654e58d1de262be3cdb1504316f7a0bf17d58ff
stanislavbratkov <st.dm.br.04@mail.ru>
2024-06-03 16:50:23 +0300

    Models for MRP

--- add_crm/add_crm/urls.py
+++ add_crm/add_crm/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('crm.urls')),
+    path('mrp/', include('mrp.urls')),
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
--- add_crm/mrp/models.py
+++ add_crm/mrp/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Purchase_topic(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=300)
+    stage = models.IntegerField(verbose_name="Этап", blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Status(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=300)
+    def __str__(self) -> str:
+        return self.title
+
+
+class Units(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=300)
+    def __str__(self) -> str:
+        return self.title
+
+class Positions(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=300)
+    quantity = models.FloatField(verbose_name="Количество")
+    units = models.ForeignKey(to="Units", on_delete=models.PROTECT)
+    def __str__(self) -> str:
+        return self.title
+
+class Application(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=300)
+    purchase_topic = models.ForeignKey(to="Purchase_topic", on_delete=models.PROTECT)
+    creator = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    status = models.ForeignKey(to="Status", on_delete=models.PROTECT)
+    positions = models.ManyToManyField("Empl", blank=True)
+    def __str__(self) -> str:
+        return self.title
+    
