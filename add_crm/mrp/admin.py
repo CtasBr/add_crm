927e9812e8ba31fceca86453e9dc758aa75c2ef0
stanislavbratkov <st.dm.br.04@mail.ru>
2024-06-03 16:55:46 +0300

    Reg

--- add_crm/mrp/admin.py
+++ add_crm/mrp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+admin.site.register(Purchase_topic)
+admin.site.register(Status)
+admin.site.register(Units)
+admin.site.register(Positions)
+admin.site.register(Application)
